4b645470a5ab49b7e494a4ef80b8da2baae1aead
ben4code <nnaemeka.obioha@gmail.com>
2021-04-13 22:21:59 +0100

    Added raw api key

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -2,7 +2,7 @@
 
 export default async (req, res) => {
 
-  const response = await fetch(`https://newsapi.org/v2/everything?q=cyber%20security&sortBy=popularity&pageSize=10&apiKey=${process.env.NEXT_PUBLIC_NEWSAPI_KEY}`)
+  const response = await fetch(`https://newsapi.org/v2/everything?q=cyber%20security&sortBy=popularity&pageSize=10&apiKey=e0e8226841c34783806528760fae44c7`)
 
   const data = await response.json()
   res.status(200).json(data)
